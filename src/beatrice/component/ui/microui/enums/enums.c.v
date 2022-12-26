44164b4efe49d22ee8274d532a584dca661430ba
xJunko <fireredz69420@gmail.com>
2022-12-26 18:00:48 +0800

    UI: use v's enum instead of `C`'s

---
+++ src/beatrice/component/ui/microui/enums/enums.c.v
@@ -0,0 +1,33 @@
+module enums
+
+pub const (
+	clip_part = 1
+	clip_all  = 2
+)
+
+pub const (
+	command_jump = 1
+	command_clip = 2
+	command_rect = 3
+	command_text = 4
+	command_icon = 5
+	command_max  = 6
+)
+
+pub const (
+	color_text        = 0
+	color_border      = 1
+	color_windowbg    = 2
+	color_titlebg     = 3
+	color_titletext   = 4
+	color_panelbg     = 5
+	color_button      = 6
+	color_buttonhover = 7
+	color_buttonfocus = 8
+	color_base        = 9
+	color_basehover   = 10
+	color_basefocus   = 11
+	color_scrollbase  = 12
+	color_scrollthumb = 13
+	color_max         = 14
+)
--- src/beatrice/component/ui/ui.v
+++ src/beatrice/component/ui/ui.v
@@ -3,6 +3,7 @@ module ui
 import gx
 import src.beatrice.component.object
 import src.beatrice.component.ui.microui
+import src.beatrice.component.ui.microui.enums
 import src.beatrice.graphic.backend
 import sokol.sgl
 
@@ -58,24 +59,25 @@ pub fn (mut ui UIManager) draw() {
 
 	for C.mu_next_command(&ui.ctx, &cmd) {
 		match cmd.@type {
-			C.MU_COMMAND_TEXT {
+			enums.command_text {
 				ui.backend.draw_text(f64(cmd.text.pos.x), f64(cmd.text.pos.y), unsafe { cstring_to_vstring(cmd.text.str) },
 					gx.TextCfg{
 					color: gx.white
 				})
 			}
-			C.MU_COMMAND_RECT {
+			enums.command_rect {
 				ui.backend.draw_rect_filled(f64(cmd.rect.rect.x), f64(cmd.rect.rect.y),
 					f64(cmd.rect.rect.w), f64(cmd.rect.rect.h), object.GameObjectColor[f64]{f64(cmd.rect.color.r), f64(cmd.rect.color.g), f64(cmd.rect.color.b), f64(cmd.rect.color.a)})
 			}
-			C.MU_COMMAND_ICON {
+			enums.command_icon {
 				// FIXME: fix this
 				x := cmd.rect.rect.x + (cmd.rect.rect.w - 16) / 2
 				y := cmd.rect.rect.y + (cmd.rect.rect.h - 16) / 2
 
 				ui.backend.draw_rect_filled(f64(x), f64(y), f64(16), f64(16), object.GameObjectColor[f64]{f64(cmd.icon.color.r), f64(cmd.icon.color.g), f64(cmd.icon.color.b), f64(cmd.icon.color.a)})
 			}
-			C.MU_COMMAND_CLIP {
+			enums.command_clip {
+				// This only works with gg-backend
 				sgl.scissor_rect(cmd.clip.rect.x, cmd.clip.rect.y, cmd.clip.rect.w, cmd.clip.rect.h,
 					true)
 			}
