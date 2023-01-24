a44a096d21037a10d237c12221cf8f03787d1a87
xJunko <fireredz69420@gmail.com>
2023-01-24 23:02:28 +0800

    Backend: Localize `StartWindowArgument`

---
+++ src/beatrice/graphic/window/backend_common.v
@@ -0,0 +1,15 @@
+module window
+
+pub type WindowFunctionCallback = fn (data voidptr)
+
+[args; params]
+pub struct StartWindowArgument {
+pub:
+	width  int
+	height int
+	// im not quite sure how to call parent init_fn so im just gonna
+	// pass it thru the start argument and be done with it
+	// ez
+	init_fn  WindowFunctionCallback
+	frame_fn WindowFunctionCallback
+}
