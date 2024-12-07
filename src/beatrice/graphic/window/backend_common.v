f02108a4d2ab3a9b9a6b88d5072f30d909bb3e19
xJunko <fireredz69420@gmail.com>
2024-12-08 00:28:43 +0800

    StartWindowArgument: Use `nil`

--- src/beatrice/graphic/window/backend_common.v
+++ src/beatrice/graphic/window/backend_common.v
@@ -2,11 +2,6 @@ module window
 
 pub type WindowFunctionCallback = fn (data voidptr)
 
-fn empty_function(data voidptr) {
-	println('StartWindowArgument: Default Callback!!!')
-	println('Check your implementation!')
-}
-
 @[args; params]
 pub struct StartWindowArgument {
 pub:
@@ -17,6 +12,6 @@ pub:
 	// im not quite sure how to call parent init_fn so im just gonna
 	// pass it thru the start argument and be done with it
 	// ez
-	init_fn  WindowFunctionCallback = empty_function
-	frame_fn WindowFunctionCallback = empty_function
+	init_fn  WindowFunctionCallback = unsafe { nil }
+	frame_fn WindowFunctionCallback = unsafe { nil }
 }
