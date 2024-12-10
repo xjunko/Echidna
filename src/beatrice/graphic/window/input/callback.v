7792fa4cb4718288a68fc57936b00d429e5e85a9
xJunko <fireredz69420@gmail.com>
2024-12-11 05:59:24 +0800

    Beatrice: Nuke old framework code

--- src/beatrice/graphic/window/input/callback.v
+++
@@ -1,7 +0,0 @@
-module input
-
-import beatrice.math.vector
-
-pub type MouseCallback = fn (state InputState, button ButtonType, pos vector.Vector2[f64])
-
-pub type KeyboardCallback = fn (state InputState, button ButtonType, pos vector.Vector2[f64])
